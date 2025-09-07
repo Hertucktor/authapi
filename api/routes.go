c8ad67dc86b015cb491503ee5126fb274dee5a19
root <vp-development@outlook.de>
2025-09-07 13:47:38 +0200

    add compose file to start up mongodb

--- api/routes.go
+++ api/routes.go
@@ -9,6 +9,8 @@ func SetupRoutes() {
 	router := gin.Default()
 
 	router.GET("/status", getStatus)
+	// router.Get("/health", getHealth) for Overall health stats on the db
+	// router.Get("/metrics", getMetrics) Uptime, Resource Usage etc.
 
 	router.Run(":8080")
 }
