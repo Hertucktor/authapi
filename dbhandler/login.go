1e21b3e2b1c801a5c9d537aad7c64a8846b3c237
root <vp-development@outlook.de>
2025-09-08 12:36:08 +0200

    add login func

---
+++ dbhandler/login.go
@@ -0,0 +1,50 @@
+package dbhandler
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func LoginUser(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var credentials struct {
+		Username string `json:"username" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+	// bind user data from gin context to credentials struct
+	if err := c.BindJSON(&credentials); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// find user in db
+	var user User
+	err := userCollection.FindOne(ctx, bson.M{"username": credentials.Username}).Decode(&user)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Ungültige Anmeldedaten"})
+		return
+	}
+
+	// check password
+	if !CheckPasswordHash(user.Password, credentials.Password) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Ungültige Anmeldedaten"})
+		return
+	}
+
+	// success message
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Erfolgreich eingeloggt",
+		"user": gin.H{
+			"id":       user.ID,
+			"name":     user.Name,
+			"Email":    user.Email,
+			"username": user.Username,
+		},
+	})
+}
