8ae752247b38e5c2740c0727a3f205429c149db3
root <vp-development@outlook.de>
2025-09-08 12:41:54 +0200

    add new login and register route

--- api/routes.go
+++ api/routes.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"log"
+
+	"github.com/Hertucktor/authapi/dbhandler"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +14,10 @@ func SetupRoutes() {
 	router.GET("/status", getStatus)
 	// router.Get("/health", getHealth) for Overall health stats on the db
 	// router.Get("/metrics", getMetrics) Uptime, Resource Usage etc.
+	router.POST("/register", dbhandler.RegisterUser)
+	router.POST("/login", dbhandler.LoginUser)
 
-	router.Run(":8080")
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Server konnte nicht gestartet werden: ", err)
+	}
 }
